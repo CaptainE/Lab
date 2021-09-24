4c377efa534b0b25b8621d3cd106825888f6ebe0
KMnP <mj493@cornell.edu>
2021-09-23 21:43:39 -0400

    added Gemfile.lock to gitignore

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #bundle install
 
-rm Gemfile.lock
+# remove cached site and folders first for a clean rendering
+rm -rf Gemfile.lock
+rm -rf _site
+rm -rf .jekyll-cache
+
 bundle exec jekyll serve
